9674e03f75b39d0ace3f14375e968a12a9e99454
kingrayhan <rayhan095@gmail.com>
2022-01-18 09:37:02 +0600

    stop screenshot taking 4

--- functions/og-image.ts
+++ functions/og-image.ts
@@ -8,9 +8,10 @@ import wait from "waait";
 const getScreenShot = async () => {
   const browser = await puppeteer.launch({
     product: "chrome",
-    executablePath: await chrome.executablePath,
+    executablePath:
+      process.env.CHROME_EXECUTEABLE_PATH || (await chrome.executablePath),
     args: chrome.args,
-    headless: true,
+    headless: chrome.headless,
     defaultViewport: {
       width: 1200,
       height: 630,
@@ -26,17 +27,27 @@ const getScreenShot = async () => {
   // const buffer = await page.screenshot({ type: "png" });
   // const base64Image = buffer.toString("base64");
   // cached.set("heyy", base64Image);
-  browser.close();
-  // return base64Image;
+
+  const headingText = await page.$eval(
+    "#__layout > main > div > div.flex-1 > h3",
+    (el) => el.textContent
+  );
+
+  console.log(headingText);
+
+  // browser.close();
+
+  return headingText;
 };
 
 export const handler: Handler = async (event, context) => {
-  // const base64Image = await getScreenShot();
+  const base64Image = await getScreenShot();
 
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: "Go Serverless v1.0! Your function executed successfully!",
+      base64Image,
     }),
     // isBase64Encoded: true,
   };
